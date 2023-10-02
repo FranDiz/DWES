1c865c15d43e862b1661f64d03fd12882e22b162
Fran_ <Fran_Diz@outlook.com>
2023-10-02 18:59:21 +0200

    ok

---
+++ src/ifSwitch02/Ejercicio1.java
@@ -0,0 +1,20 @@
+package ifSwitch02;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a weekday:");
+        String weekDay = scanner.next().toLowerCase();
+        String subject = switch (weekDay) {
+            case "monday" -> "DWES";
+            case "tuesday" -> "HLC";
+            case "wednesday" -> "DWEC";
+            case "thursday" -> "EIE";
+            case "friday" -> "DWEC";
+            default -> "You have no classes that day at first hour";
+        };
+        System.out.println(subject);
+    }
+}
